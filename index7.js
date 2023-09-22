d1e552c38fde5f22a7ca241f9c5b75681fdd2876
TheCoderAishwarya <113344237+TheCoderAish@users.noreply.github.com>
2023-09-23 00:36:32 +0530

    Add files via upload

---
+++ index7.js
@@ -0,0 +1,9 @@
+let text = document.querySelectorAll('ul li a').forEach(text => {
+    text.innerHTML = text.innerText.split('').map((letters, i) => `<span style="transition-delay:${i*40}ms;">${letters}</span>`).join('');
+})
+
+let cursor = document.querySelector('#cursor');
+document.addEventListener('mousemove', (e) => {
+    cursor.style.top = e.pageY + 'px';
+    cursor.style.left = e.pageX + 'px';
+})
